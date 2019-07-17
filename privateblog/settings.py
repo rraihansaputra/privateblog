d9432d83f9caa87bdd9f547b9c396fc8a582b49e
Raihan Saputra <rraihansaputra@gmail.com>
2019-07-17 16:04:56 +0700

    added gunicorn and whitenoise to depedencies; addded whitenoise configs

--- privateblog/settings.py
+++ privateblog/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,8 +130,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    "static"
+    os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+#STATICFILES_DIRS = [
+#    "static"
+#]
 
 # Changes for custom user model
 # refer to below for config changes (email login, etc)
